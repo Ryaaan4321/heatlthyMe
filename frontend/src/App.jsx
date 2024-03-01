75be00eddb8c9dde1f0d9f4f7665626a9b7aad14
Ryaaan4321 <aryan056400@gmail.com>
2024-03-01 10:35:38 +0530

    go commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+    hello hello hello
+      
+      </>
+  )
+}
+
+export default App
